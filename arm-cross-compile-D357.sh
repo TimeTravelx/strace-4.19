a0737b81722e2b39f9e3a288982ae57ceccaa062
huangyongtao <huangyongtao@hangsheng.com.cn>
2021-01-12 11:01:17 +0800

    修改D357交叉编译脚本

--- arm-cross-compile-D357.sh
+++ arm-cross-compile-D357.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+make distclean
 source /home/joshua/TBOX_SDK/D357/LinuxT/ql-ol-crosstool/ql-ol-crosstool-env-init
-./configure
+./configure --target=arm-oe-linux-gnueabi --host=arm-oe-linux-gnueabi --build=x86_64-linux
+make -j4
